5557fb907ab18b6779febf4a1ffd8c9c1a3e55d7
YaremaV <Yaremenko1996v@gmail.com>
2021-08-01 19:47:07 +0300

    Добавил код для смены цвета фона body

--- index.js
+++ index.js
@@ -7,6 +7,39 @@ const colors = [
   '#795548',
 ];
 
-const randomIntegerFromInterval = (min, max) => {
-  return Math.floor(Math.random() * (max - min + 1) + min);
+const COLORS_DELAY = 1000;
+let timeOutId = null;
+
+const refs = {
+  start: document.querySelector('button[data-action="start"]'),
+  stop: document.querySelector('button[data-action="stop"]')
 }
+console.log(refs.start)
+console.log(refs.stop)
+
+refs.start.addEventListener('click', () => {
+  console.log('start')
+  
+  startBgColor()
+  refs.start.setAttribute('disabled', true);
+});
+
+refs.stop.addEventListener('click', () => {
+  console.log('stop')
+  clearInterval(timeOutId);
+  refs.start.removeAttribute('disabled');
+});
+
+function setBodyBgColor() {
+
+const randomColor = colors[Math.floor(Math.random() * colors.length)]
+     document.body.style.backgroundColor = randomColor;
+}
+
+function startBgColor() {
+  timeOutId = setInterval(setBodyBgColor, COLORS_DELAY);
+}
+
+// const randomIntegerFromInterval = (min, max) => {
+//   return Math.floor(Math.random() * (max - min + 1) + min);
+// }
